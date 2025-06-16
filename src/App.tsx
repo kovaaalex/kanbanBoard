2b6f2eb31517a4b94e37f57111d134fc72f14f5e
kovaaalex <kovalenko.alex04@gmail.com>
2025-06-16 10:00:44 +0300

    add errorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import '@fontsource/plus-jakarta-sans/800.css';
 import './App.css';
 import Header from './components/Header/Index';
 import KanbanBoard from './components/KanbanBoard/Index';
+import ErrorBoundary from './components/ErrorBoundary';
 
 function App() {
   return (
-    <>
+    <ErrorBoundary
+      fallback={<p>Что-то пошло не так, перезапустите страницу</p>}
+    >
       <Header />
       <KanbanBoard />
-    </>
+    </ErrorBoundary>
   );
 }
 
---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,35 @@
+import { Component, type ErrorInfo, type ReactNode } from 'react';
+
+interface Props {
+  children?: ReactNode;
+  fallback?: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  public static getDerivedStateFromError(): State {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error('ErrorBoundary caught an error: ', error, errorInfo);
+    this.setState({ hasError: true });
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
